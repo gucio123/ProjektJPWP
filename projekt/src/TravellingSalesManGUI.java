f9827d270ceaab3c4f69aa7e0c597759d797a726
veeexy <marasek515@gmail.com>
2022-03-02 22:09:32 +0100

    baza

--- projekt/src/TravellingSalesManGUI.java
+++ projekt/src/TravellingSalesManGUI.java
@@ -7,16 +7,20 @@ import java.io.File;
 import java.io.IOException;
 import java.awt.*;
 import java.awt.geom.*;
+import java.awt.*;
 
-public class TravellingSalesManGUI {
+public class TravellingSalesManGUI extends JFrame {
     private JPanel panel1;
     private JButton button1;
     private JButton button2;
     private JButton button3;
     private JPanel mapa;
     private JLabel mapLabel;
+    private Image map;
 
     public TravellingSalesManGUI() {
+        ImageIcon mapa = new ImageIcon("PROJEKT/map/mapka.png");
+        map = mapa.getImage();
         mapLabel.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -26,11 +30,21 @@ public class TravellingSalesManGUI {
             }
         });
     }
+        @Override
+        public void paint(Graphics g){
+            Graphics2D g2 = (Graphics2D) g;
+            g2.drawImage(map,0,0,null);
+            g2.setColor(Color.RED);
+//            g2.drawLine(77, 268, 237, 390);
+        }
+
     public static void main(String[] args) throws IOException {
-        JFrame frame = new JFrame("TravellingSalesManGUI");
+//        JFrame frame = new JFrame("TravellingSalesManGUI");
+        TravellingSalesManGUI frame = new TravellingSalesManGUI();
         frame.setContentPane(new TravellingSalesManGUI().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+        frame.setSize(798,570);
     }
 }
