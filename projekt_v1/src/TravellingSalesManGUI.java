745c23e4b0f78a90f86ee0e19b009136b6b71d8d
BGutowski <bartek.gutowski@onet.pl>
2022-03-02 17:40:58 +0100

    firstCommit

--- projekt_v1/src/TravellingSalesManGUI.java
+++ projekt_v1/src/TravellingSalesManGUI.java
@@ -5,6 +5,8 @@ import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.awt.*;
+import java.awt.geom.*;
 
 public class TravellingSalesManGUI {
     private JPanel panel1;
@@ -20,10 +22,15 @@ public class TravellingSalesManGUI {
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
                 System.out.println(e.getX() + " " + e.getY());
+
             }
         });
     }
-
+    public void paint(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        Line2D lin = new Line2D.Float(100, 100, 250, 260);
+        g2.draw(lin);
+    }
     public static void main(String[] args) throws IOException {
         JFrame frame = new JFrame("TravellingSalesManGUI");
         frame.setContentPane(new TravellingSalesManGUI().panel1);
