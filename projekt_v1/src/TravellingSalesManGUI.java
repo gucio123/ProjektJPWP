e747956db305d86861bc813c5051709796b627fb
BGutowski <bartek.gutowski@onet.pl>
2022-03-02 17:43:25 +0100

    firstCommit

--- projekt_v1/src/TravellingSalesManGUI.java
+++ projekt_v1/src/TravellingSalesManGUI.java
@@ -26,11 +26,6 @@ public class TravellingSalesManGUI {
             }
         });
     }
-    public void paint(Graphics g) {
-        Graphics2D g2 = (Graphics2D) g;
-        Line2D lin = new Line2D.Float(100, 100, 250, 260);
-        g2.draw(lin);
-    }
     public static void main(String[] args) throws IOException {
         JFrame frame = new JFrame("TravellingSalesManGUI");
         frame.setContentPane(new TravellingSalesManGUI().panel1);
