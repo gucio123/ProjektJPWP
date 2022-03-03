34a3d349bb6725aeed0e684b82731d74b82d1177
veeexy <marasek515@gmail.com>
2022-03-03 23:44:23 +0100

    delay linii

--- projekt/src/TravellingSalesManGUI.java
+++ projekt/src/TravellingSalesManGUI.java
@@ -1,4 +1,5 @@
 import javax.imageio.ImageIO;
+import javax.print.attribute.standard.RequestingUserName;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,32 +14,30 @@ import java.awt.*;
 
 public class TravellingSalesManGUI extends JFrame {
 
-    private JButton Losowo;
-    private JButton button2;
-    private JButton button3;
+    private JButton Losowo = new JButton("Kliknij");
+    public Graphics2D g2;
     private JPanel mapPanel;
     private JLabel mapLabel;
     private Image map;
     private boolean paintcheck = true;
     private TravellingSalesManRandom random = new TravellingSalesManRandom("Waszyngton");
+    private int a = 0;
+    private int b = 0;
+    private int c = 0;
+    private int d = 0;
 
-    private JPanel panel1 = new JPanel(){
+    private JPanel panel1 = new JPanel() {
         @Override
-        public void paintComponent(Graphics g) {
-//            super.paintComponent(g);
+        public void paint(Graphics g) {
+            super.paintComponent(g);
             Graphics2D g2 = (Graphics2D) g;
             g2.drawImage(map, 0, 0, null);
             g2.setColor(Color.RED);
-            if (paintcheck) {
-                for (int i = 0; i < random.getFinalLIst().toArray().length - 1; i++) {
-                    g2.drawLine(random.getFinalLIst().get(i).getX(), random.getFinalLIst().get(i).getY(), random.getFinalLIst().get(i + 1).getX(), random.getFinalLIst().get(i + 1).getY());
-                }
-            }
+            g2.drawLine(a, b, c, d);
         }
     };
 
     public TravellingSalesManGUI() {
-
         ImageIcon mapa = new ImageIcon("PROJEKT/map/mapka.png");
         map = mapa.getImage();
         mapPanel.addMouseListener(new MouseAdapter() {
@@ -52,12 +51,31 @@ public class TravellingSalesManGUI extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 random.way();
-                paintcheck = true;
                 panel1.repaint();
+                Runnable paintController = new Runnable() {
+                    @Override
+                    public void run() {
+                        for (int i = 0; i < random.getFinalLIst().toArray().length - 1; i++) {
+                            try {
+                                Thread.sleep(500);
+                                a = random.getFinalLIst().get(i).getX();
+                                b = random.getFinalLIst().get(i).getY();
+                                c = random.getFinalLIst().get(i + 1).getX();
+                                d = random.getFinalLIst().get(i + 1).getY();
+                                panel1.repaint();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                };
+                Thread paintThread = new Thread(paintController);
+                paintThread.start();
 
             }
 
         });
+//        Losowo.setBounds(399,550,100,50);
         panel1.add(Losowo);
     }
 
