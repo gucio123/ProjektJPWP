c8f01ad267f88f5c876ac5b02f6e2273a587a081
veeexy <marasek515@gmail.com>
2022-03-17 21:01:10 +0100

    RYSUJE, ALE Z MIASTA DO MIASTAXD

--- projekt/src/TravellingSalesManGUI.java
+++ projekt/src/TravellingSalesManGUI.java
@@ -22,6 +22,10 @@ public class TravellingSalesManGUI extends JFrame {
     private boolean paintcheck = false;
     private boolean geneticCheck = false;
     private int index = -1;
+    private int a = 0;
+    private int b = 0;
+    private int c = 0;
+    private int d = 0;
     private TravellingSalesManRandom random = new TravellingSalesManRandom("Waszyngton");
     private GeneticTravellingSalesman genetic = new GeneticTravellingSalesman();
 
@@ -46,16 +50,22 @@ public class TravellingSalesManGUI extends JFrame {
                 g2.setColor(Color.BLACK);
                 g2.drawString(String.valueOf(random.getWayLenght()), 100, 10);
                 g2.setColor(Color.RED);
+
             }
+
+
             if (geneticCheck) {
-                    for (int j = 0; j < genetic.getList().get(index).toArray().length - 1; j++) {
-                            g2.drawLine(((City) genetic.getList().get(index).get(j)).getX(),
-                                    ((City) genetic.getList().get(index).get(j)).getY(),
-                                    ((City) genetic.getList().get(index).get(j + 1)).getX(),
-                                    ((City) genetic.getList().get(index).get(j + 1)).getY());
-                    }
+                g2.drawLine(a, b, c, d);
             }
-        }};
+
+//                    for (int j = 0; j < genetic.getList().get(index).toArray().length - 1; j++) {
+//                            g2.drawLine(((City) genetic.getList().get(index).get(j)).getX(),
+//                                    ((City) genetic.getList().get(index).get(j)).getY(),
+//                                    ((City) genetic.getList().get(index).get(j + 1)).getX(),
+//                                    ((City) genetic.getList().get(index).get(j + 1)).getY());
+//
+        }
+    };
 
     public TravellingSalesManGUI() {
         ImageIcon mapa = new ImageIcon("PROJEKT/map/mapka.png");
@@ -71,16 +81,42 @@ public class TravellingSalesManGUI extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 genetic.firstPopulation();
+                panel1.repaint();
                 geneticCheck = true;
-                for(int i = 0; i < 100; i++) {
-                    index = i;
-                    try{
-                        Thread.sleep(1);
-                        panel1.repaint();
-                    }catch (InterruptedException exc){}
+                Runnable paintController = new Runnable() {
+                    @Override
+                    public void run() {
+                        for (int k = 0; k < 100; k++) {
+                            index = k;
+                            for (int i = 0; i < genetic.getList().get(index).toArray().length - 1; i++) {
+                                try {
+                                    Thread.sleep(100);
+                                    if (genetic.getList().get(index).get(i) instanceof City) {
+                                        a = ((City) genetic.getList().get(index).get(i)).getX();
+                                        b = ((City) genetic.getList().get(index).get(i)).getY();
+                                        c = ((City) genetic.getList().get(index).get(i + 1)).getX();
+                                        d = ((City) genetic.getList().get(index).get(i + 1)).getY();
+                                        panel1.repaint();
+                                    }
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    }
+                };
+                Thread paintThread = new Thread(paintController);
+                paintThread.start();
 
-                }
+//                for(int i = 0; i < 100; i++) {
+//                    index = i;
+//                    try{
+//                        Thread.sleep(1);
+//                        panel1.repaint();
+//                    }catch (InterruptedException exc){}
+
             }
+
         });
         Losowo.addActionListener(new ActionListener() {
             @Override
