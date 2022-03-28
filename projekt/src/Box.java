c9ebd7f375b55e7810d520009c46db188bd2ed07
veeexy <marasek515@gmail.com>
2022-03-28 21:12:56 +0200

    zachlanny almost done

--- projekt/src/Box.java
+++ projekt/src/Box.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Box {
+/*public class Box {
 
     public static List<Box> listOfBoxes = new ArrayList<Box>(Arrays.asList(new Box(1,1,3), new Box(2,1,4), new Box()));
 
@@ -15,3 +15,4 @@ public class Box {
     }
 
 }
+*/
