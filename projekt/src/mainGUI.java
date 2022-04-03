d98c3ca8267fbf16539984ad67649ebcb0ae3aad
veeexy <marasek515@gmail.com>
2022-04-03 18:56:04 +0200

    gui

---
+++ projekt/src/mainGUI.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class mainGUI extends JFrame{
+    private JPanel panel1;
+    private JButton backpackButton;
+    private JButton TravellingSalesmanButton;
+    private JButton chujbutton3;
+
+}
